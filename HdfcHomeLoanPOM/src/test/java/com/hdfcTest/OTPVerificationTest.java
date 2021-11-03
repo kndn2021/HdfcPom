24df1003a425d4151b5e465eee256fe34cb894fc
Bhushan <Bhushan@LAPTOP-PE52CNM5>
2021-11-03 20:49:32 +0530

    OTP Bypass

--- HdfcHomeLoanPOM/src/test/java/com/hdfcTest/OTPVerificationTest.java
+++ HdfcHomeLoanPOM/src/test/java/com/hdfcTest/OTPVerificationTest.java
@@ -1,7 +1,6 @@
 package com.hdfcTest;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -18,7 +17,7 @@ public class OTPVerificationTest extends HdfcDrivers {
 	
 	ApplicantsTest app_test;
 	OTPVerificationPage otp_verify;
-	Scanner scanner;
+//	Scanner scanner;
 	ScrollUtility scroll;
 	Logger log;
 	
@@ -42,7 +41,7 @@ public class OTPVerificationTest extends HdfcDrivers {
 		scroll = new ScrollUtility();
 		
 		otp_verify = new OTPVerificationPage();
-		scanner = new Scanner(System.in);
+//		scanner = new Scanner(System.in);
 	}
 	
 	@Test
@@ -55,13 +54,14 @@ public class OTPVerificationTest extends HdfcDrivers {
 		
 		log.info("Clicked on Resend OTP button");
 		
-		System.out.println("Please enter the OTP that you received on mobile number..");
-		String newOtp = scanner.next();
+//		System.out.println("Please enter the OTP that you received on mobile number..");
+//		String newOtp = scanner.next();
 		
 		log.warn("Please do check the entered OTP carefully");
 		
 		Thread.sleep(2000);
-		otp_verify.enterOTP(newOtp);
+//		otp_verify.enterOTP(newOtp);
+		otp_verify.enterOTP("4405");
 		
 		log.info("OTP entered successfully");
 		
