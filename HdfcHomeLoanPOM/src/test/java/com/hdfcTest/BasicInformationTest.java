6dce136fa91699e51955bb921770bbdc7ed2c3fb
Bhushan <Bhushan@LAPTOP-PE52CNM5>
2021-11-03 20:41:19 +0530

    This is v1.2

--- HdfcHomeLoanPOM/src/test/java/com/hdfcTest/BasicInformationTest.java
+++ HdfcHomeLoanPOM/src/test/java/com/hdfcTest/BasicInformationTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -64,9 +65,9 @@ public class BasicInformationTest extends HdfcDrivers {
 		log.info("Clicked on Basic information continue button");
 	}
 	
-//	@AfterMethod
-//	public void tearDown()
-//	{
-//		driver.close();
-//	}
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.close();
+	}
 }
