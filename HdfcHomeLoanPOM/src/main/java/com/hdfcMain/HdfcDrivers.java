9135cfd07e6ada9b23ee4469d59dc6640a0d2343
Bhushan <Bhushan@LAPTOP-PE52CNM5.hdfc.com>
2021-12-14 16:31:13 +0530

    Commit property file configurations

--- HdfcHomeLoanPOM/src/main/java/com/hdfcMain/HdfcDrivers.java
+++ HdfcHomeLoanPOM/src/main/java/com/hdfcMain/HdfcDrivers.java
@@ -17,31 +17,32 @@ public class HdfcDrivers {
 	
 	public HdfcDrivers() throws IOException
 	{
-		prop = new Properties();
-		file_input = new FileInputStream("C:\\Users\\Bhushan\\eclipse-workspace\\HdfcHomeLoanPOM\\src\\main\\resources\\HdfcConfig.properties");
-		prop.load(file_input);
+//		prop = new Properties();
+//		file_input = new FileInputStream("C:\\Users\\Bhushan\\eclipse-workspace\\HdfcHomeLoanPOM\\src\\main\\resources\\HdfcConfig.properties");
+//		prop.load(file_input);
 	}
 	
 	public void initDrivers()
 	{	
-		String browser = prop.getProperty("Browser_Type");
-		if(browser.equalsIgnoreCase("Chrome"))
-		{
+	//	String browser = prop.getProperty("Browser_Type");
+	//	if(browser.equalsIgnoreCase("Chrome"))
+//		{
 			System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 			System.setProperty("webdriver.chrome.driver", "C:\\Automation\\Driver\\chromedriver.exe");
 			driver = new ChromeDriver();
-		}
-		else
-		{
-			System.out.println("=========No browser found==========");
-			driver.quit();
-		}
+	//	}
+//		else
+//		{
+//			System.out.println("=========No browser found==========");
+//			driver.quit();
+//		}
 		
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get(prop.getProperty("App_Url"));
+	//	driver.get(prop.getProperty("App_Url"));
+		driver.get("https://portal.hdfc.com/campaign/new?utm_agency=Metier&utm_source=Google&utm_medium=Text-Ad&loantype=NEW&gclid=EAIaIQobChMIkbGXx6jo8wIVBjUrCh0K8QJuEAAYASAAEgJIxPD_BwE");
 	}
 	
 }
